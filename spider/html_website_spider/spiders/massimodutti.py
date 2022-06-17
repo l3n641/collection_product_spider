6655a210c46c2b42a7fc34b188bec9ed4fa415e9
l3n641 <371522155@qq.com>
2022-06-17 22:05:28 +0800

    add massimodutti_fr 爬虫

--- spider/html_website_spider/spiders/massimodutti.py
+++ spider/html_website_spider/spiders/massimodutti.py
@@ -10,6 +10,7 @@ class Massimodutti(CommonSpider):
     name = 'massimodutti'
     allowed_domains = ['www.massimodutti.com']
     BASE_URL = "https://www.massimodutti.com/"
+    detail_api_url = 'https://www.massimodutti.com/itxrest/2/catalog/store/34009456/30359502/category/0/product/{}/detail?languageId=-1&appId=1'
 
     def parse_product_list(self, response, **kwargs):
         pattern = '<li><a href="(.*?)"><img'
@@ -29,7 +30,7 @@ class Massimodutti(CommonSpider):
     def parse_product_detail(self, response, **kwargs):
         pattern = "inditex.iProductId = (\d+);"
         product_id = response.selector.re_first(pattern)
-        detail_api_url = f'https://www.massimodutti.com/itxrest/2/catalog/store/34009456/30359502/category/0/product/{product_id}/detail?languageId=-1&appId=1'
+        detail_api_url = self.detail_api_url.format(product_id)
         if product_id:
             response.meta['url'] = response.url
             yield scrapy.Request(url=detail_api_url, meta=response.meta, callback=self.parse_product_extra_data)
@@ -84,6 +85,6 @@ class Massimodutti(CommonSpider):
 
                 file_data = data.get("xmediaLocations")[0].get("locations")[1].get("mediaLocations")
                 for file in file_data:
-                    url = f"{base_url}{path}/{file}16.jpg"
+                    url = f"{base_url}{path}/{file}16.jpg?imwidth=700"
                     images.append(url)
                 return images
