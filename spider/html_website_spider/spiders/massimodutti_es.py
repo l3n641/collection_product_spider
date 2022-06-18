615795bee58b339aaf3b28bd244ffc29ac4f22a0
l3n641 <371522155@qq.com>
2022-06-18 09:17:12 +0800

    add massimodutti_es spider

---
+++ spider/html_website_spider/spiders/massimodutti_es.py
@@ -0,0 +1,6 @@
+from .massimodutti import Massimodutti
+
+
+class MassimoduttiEs(Massimodutti):
+    name = 'massimodutti_es'
+    detail_api_url = "https://www.massimodutti.com/itxrest/2/catalog/store/34009450/30359536/category/1940002/product/{}/detail?languageId=-5&appId=1"
