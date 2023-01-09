8ebd5e5c7bfb3d5ca3fa1afc26d60733b672701e
l3n641 <371522155@qq.com>
2023-01-09 10:34:43 +0800

    update  the scotch-soda spider

--- spider/html_website_spider/spiders/scotch_soda/scotch_soda_nl.py
+++ spider/html_website_spider/spiders/scotch_soda/scotch_soda_nl.py
@@ -5,17 +5,16 @@ from ..common_spider import CommonSpider
 from .. import ProductUrlItem, ProductDetailItem
 from datetime import datetime
 
-from urllib.parse import urlparse
-import requests
-import re
-
 
 class ScotchSodaNlSpider(CommonSpider):
     name = 'scotchsoda_nl'
     allowed_domains = ['scotch-soda.com']
     base_url = "https://www.scotch-soda.com"
-    category_url = "https://www.burton.com/us/en"
-    detail_url = "https://www.burton.com/on/demandware.store/Sites-Burton_NA-Site/en_US/Product-Variation?dwvar_{}_variationColor={}&pid={}&quantity=1"
+
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 4,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 2,
+    }
 
     def parse_product_list(self, response, **kwargs):
         product_url_xpath = '//div[@class="product-trigger__media"]/a'
@@ -40,27 +39,28 @@ class ScotchSodaNlSpider(CommonSpider):
 
         next_page_node = response.xpath(next_page_xpath)
         if next_page_node:
-            next_url = self.base_url + next_page_node.attrib.get("href")
+            next_url = next_page_node.attrib.get("href")
             yield scrapy.Request(next_url, meta=response.meta, callback=self.parse_product_list,
                                  errback=self.start_request_error)
 
     def parse_product_detail(self, response):
         img_xpath = "//li[contains(@class,'carousel--pdp__item')]/a"
         size_xpath = '//option[@data-size]'
-        product_info_xpath = '//script[@type="application/ld+json]'
-        color_xpath = '//meta[@property="product:color]'
+        product_info_xpath = '//script[@type="application/ld+json"]'
+        color_xpath = '//span[@id="gtm_product_data"]'
 
-        data_str = response.xpath(product_info_xpath)[0]
+        data_str = response.xpath(product_info_xpath)[0].root.text.strip()
         product_data = json.loads(data_str)
         title = product_data.get("name")
-        sku = product_data.get("sku")
         description = product_data.get("description")
         price = product_data.get("offers").get('price')
-        color = response.xpath(color_xpath).attrib.get("content")
-
+        data_gtm_product_data_str = response.xpath(color_xpath).attrib.get("data-gtm-product-data")
+        data_gtm_product_data = json.loads(data_gtm_product_data_str)
+        color = data_gtm_product_data.get("variant")
+        sku = data_gtm_product_data.get("variantGroupId")
         images = []
         for node in response.xpath(img_xpath):
-            src = node.attrib.get("href").replace("?sw=1300")
+            src = node.attrib.get("href").replace("?sw=1125", "?sw=1300")
             images.append(src)
 
         sizes = []
@@ -85,7 +85,7 @@ class ScotchSodaNlSpider(CommonSpider):
 
         yield ProductDetailItem(**item_data)
 
-    def start_requests(self):
-        url = 'https://www.scotch-soda.com/nl/nl/dames/kleding/truien-vesten/geborstelde-trui-met-bloemendessin/169639.html?dwvar_169639_color=Caramel&cgid=1041'
+    def start_requests1(self):
+        url = 'https://www.scotch-soda.com/nl/nl/heren/kleding/essentials/sweater-met-ronde-hals-van-biologisch-katoen/165318.html?dwvar_165318_color=Grey%20Melange&cgid=76'
         yield scrapy.Request(url, callback=self.parse_product_detail, errback=self.start_request_error,
                              dont_filter=True)
